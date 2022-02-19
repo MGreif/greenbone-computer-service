57982af6d616ea50400745f3a3fcfcd7a6408b63
Mika Greif <mika.greif@googlemail.com>
2022-02-19 15:43:34 +0100

    Add initial test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Express works!');
+router.get('/', function(req, res) {
+  res.send('greenbone-computer-service works!');
 });
 
 module.exports = router;
